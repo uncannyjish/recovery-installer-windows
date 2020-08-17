67ff2caef7ae5b3fc0c9a03ba9b22188e4813ca0
uncannyjish <43221561+uncannyjish@users.noreply.github.com>
2020-08-17 23:02:41 +0530

    Simplified Code

--- Batch/fix.bat
+++ Batch/fix.bat
@@ -1,20 +1,20 @@
 @echo off
 
-:: TWRP Installer 0.1 by @uncannyjish
+:: TWRP Installer 0.5 by @uncannyjish
 
-title System Destroyed Fix 0.2
+title System Destroyed Fix 0.5
 
 echo Detecting you device(should be on fastboot screen[Power+volume down])... 
 
-"%~dp0..\adb-fastboot\fastboot.exe" getvar product 2>&1 | findstr /r /c:"^product: *%1" || echo Missmatching image and device
-"%~dp0..\adb-fastboot\fastboot.exe" getvar product 2>&1 | findstr /r /c:"^product: *%1" || exit /B 1
+fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || echo Missmatching image and device
+fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || exit /B 1
 
-"%~dp0..\adb-fastboot\fastboot.exe" --disable-verity --disable-verification flash vbmeta "%~dp0..\images\vbmeta.img" || @echo "Flash vbmeta error" && exit /B 1
+fastboot --disable-verity --disable-verification flash vbmeta "%~dp0..\images\vbmeta.img" || @echo "Flash vbmeta error" && exit /B 1
 
 echo Issue solved... Flashing recovery and booting...
 
-"%~dp0..\adb-fastboot\fastboot.exe" flash recovery "%~dp0..\images\%1.img" || @echo "Flash recovery error" && exit /B 1
+fastboot flash recovery "%~dp0..\images\%1.img" || @echo "Flash recovery error" && exit /B 1
 
-"%~dp0..\adb-fastboot\fastboot.exe" boot "%~dp0..\images\%1.img"
+fastboot boot "%~dp0..\images\%1.img"
 
 pause
--- Batch/install.bat
+++ Batch/install.bat
@@ -1,18 +1,18 @@
 @echo off
 
-:: TWRP Installer 0.2 by @uncannyjish
+:: TWRP Installer 0.5 by @uncannyjish
 
-title TWRP Installer 0.2
+title TWRP Installer 0.5
 
 echo Detecting your device(should be on fastboot screen[Power+volume down])... 
 
-"%~dp0..\adb-fastboot\fastboot.exe" getvar product 2>&1 | findstr /r /c:"^product: *%1" || echo Missmatching image and device
-"%~dp0..\adb-fastboot\fastboot.exe" getvar product 2>&1 | findstr /r /c:"^product: *%1" || exit /B 1
+fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || echo Missmatching image and device
+fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || exit /B 1
 
-"%~dp0..\adb-fastboot\fastboot.exe" flash recovery "%~dp0..\images\%1.img" || @echo "Flash recovery error" && exit /B 1
+fastboot flash recovery "%~dp0..\images\%1.img" || @echo "Flash recovery error" && exit /B 1
 
 echo Install Successful!!! Booting to recovery...
 
-"%~dp0..\adb-fastboot\fastboot.exe" boot "%~dp0..\images\%1.img"
+fastboot boot "%~dp0..\images\%1.img"
 
 pause
