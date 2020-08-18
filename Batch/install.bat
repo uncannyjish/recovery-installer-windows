325ea759e07cbabc8b36c355df0dc9f62c9e1745
uncannyjish <43221561+uncannyjish@users.noreply.github.com>
2020-08-18 21:34:29 +0530

    Added Whyred Dummy Image

--- Batch/install.bat
+++ Batch/install.bat
@@ -9,6 +9,10 @@ echo Detecting your device(should be on fastboot screen[Power+volume down])...
 fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || echo Missmatching image and device
 fastboot getvar product 2>&1 | findstr /r /c:"^product: *%1" || exit /B 1
 
+if "%1" == "whyred" (
+fastboot flash antirbpass %~dp0..\images\dummy_whyred.img || @echo "Flash antirbpass error" && exit /B 1
+)
+
 fastboot flash recovery "%~dp0..\images\%1.img" || @echo "Flash recovery error" && exit /B 1
 
 echo Install Successful!!! Booting to recovery...
